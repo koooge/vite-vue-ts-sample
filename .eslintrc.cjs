4c50f01ba4b433372808d3d9a33ee623318f9337
koooge <koooooge@gmail.com>
2022-01-05 07:32:36 +0100

    chore: Setup lint
    Signed-off-by: koooge <koooooge@gmail.com>

---
+++ .eslintrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  env: {
+    'vue/setup-compiler-macros': true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-essential',
+    '@vue/eslint-config-typescript',
+  ],
+};
